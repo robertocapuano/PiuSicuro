1ccb4041c430cab1282cc44dc38bf36373b6c8d5
MeroniS <MeroniS@corvallis.it>
2018-02-16 14:40:09 +0100

    creo pagina garanzia

---
+++ src/pages/garanzia/garanzia.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GaranziaPage } from './garanzia';
+
+@NgModule({
+  declarations: [
+    GaranziaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GaranziaPage),
+  ],
+})
+export class GaranziaPageModule {}
