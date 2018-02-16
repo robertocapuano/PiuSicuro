d53d4cee7f01cf36020419a813378e18bd89df78
MeroniS <MeroniS@corvallis.it>
2018-02-16 12:05:58 +0100

    ts pagine

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { VeicoloPage } from '../veicolo/veicolo';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    navCtrl.push(VeicoloPage);
 
   }
 
