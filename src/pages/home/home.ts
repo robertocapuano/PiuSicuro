fa69d045fe9f41ae8dc74e48fe5851cea785fc07
Bernardo Pujada <bernardopujada>
2018-02-16 12:15:32 +0100

    package

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  marca:string="";
+  modello:string="";
+  allestimento:string="";
+  annoI:string="";
+  cilindrata:string="";
+  abilitato:boolean=false;
+
+  validaForm(){
+    if(this.marca===''||
+    this.modello==='' ||
+    this.allestimento==='' ||
+    this.annoI==='' ||
+    this.cilindrata===''){
+    this.abilitato=false;
+    }
+    else{
+      this.abilitato=true;
+    }
+  
+  }
+  
+
   constructor(public navCtrl: NavController) {
+    
+    
+  }
+
+   
+    
 
   }
 
-}
+
