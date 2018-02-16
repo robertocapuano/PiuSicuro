dbc98a821fd9b984a48333362aae562c66379360
MeroniS <MeroniS@corvallis.it>
2018-02-16 17:31:47 +0100

    commit

--- src/providers/crea-preventivo/crea-preventivo.ts
+++ src/providers/crea-preventivo/crea-preventivo.ts
@@ -40,9 +40,6 @@ export interface Preventivo
 @Injectable()
 export class CreaPreventivoProvider {
 
-  garanzie: string = "furto,incendio,minikasko,cristalli";
-  codiciGaranzie: string="1235,1478,4587,9654";  
-
 
   veicolo : Veicolo = {
 
