d06940417be9d6e1f9c92e3bf93c4adee7cf67a6
MeroniS <MeroniS@corvallis.it>
2018-02-16 15:05:17 +0100

    garanzia ts (forse pronta)

--- src/pages/garanzia/garanzia.ts
+++ src/pages/garanzia/garanzia.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CreaPreventivoProvider , Garanzia} from '../../providers/crea-preventivo/crea-preventivo';
+
 
 /**
  * Generated class for the GaranziaPage page.
@@ -14,8 +16,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'garanzia.html',
 })
 export class GaranziaPage {
+  garanzieValide;
+  @Input()
+  garanzia : Garanzia ={nome:"",codice:null};
+  garanzie: Garanzia[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public servizio : CreaPreventivoProvider) {
+    this.garanzieValide=servizio.garanzie.split(",");
+  }
+
+  addGaranzia(item){
+    this.garanzie.push(item);
+
   }
 
   ionViewDidLoad() {
