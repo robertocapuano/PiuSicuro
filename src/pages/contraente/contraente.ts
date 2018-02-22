a376dd08c02038dabd8c3a8d1afdde6d142e8c89
valentinaspera <34567953+valentinaspera@users.noreply.github.com>
2018-02-22 12:02:18 +0100

    contraente controlli

--- src/pages/contraente/contraente.ts
+++ src/pages/contraente/contraente.ts
@@ -18,16 +18,16 @@ export class ContraentePage {
   @Input()
     disabilitato=true;
 
- form : FormGroup;
+ form: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public servizo : CreaPreventivoProvider, public formBuilder: FormBuilder) {
     this.contraente=this.navParams.data;
     this.form= formBuilder.group({ 
       nome : ['',Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
       cognome: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-      dataN: ['',Validators.compose([Validators.minLength(10),Validators.maxLength(10),Validators.required]) ],
+      dataN: ['',Validators.compose([Validators.minLength(10),Validators.maxLength(10),Validators.required])],
       luogoN:['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-      anniPat:['',Validators.compose([Validators.min(1),Validators.max(65),Validators.required]) ]  });
+      anniPat:['',Validators.compose([Validators.min(1),Validators.max(65),Validators.pattern('[0-9]*'),Validators.required]) ]  });
   }
 
   ionViewDidLoad() {
